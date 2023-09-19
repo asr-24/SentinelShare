e619c3e93856c69bf5c172c9693a3be784dc7eea
Arushi <arushisr24@gmail.com>
2023-09-19 23:06:24 +0530

    data sharing between mongodb async fxns & helia

--- truffle/atlas/app_trial_two.js
+++ truffle/atlas/app_trial_two.js
@@ -10,7 +10,7 @@ const CLUSTER_URL = process.env.cluster_url;
 
 let uri = `mongodb+srv://${USER}:${PASSWORD}@${CLUSTER_URL}?retryWrites=true&w=majority`;
 
-
+let queryDataJSON = [];
 
 const client = new MongoClient(uri);
 
@@ -23,17 +23,44 @@ async function run() {
 
     const database = client.db("sentinelShare");
     const collection = database.collection("authentication");
-    console.log("\n\n\n\n\nOUTPUT:\n\n");
-    const cursor = await collection.find(findQuery).sort({ name: 1 });
+    // console.log("\n\n\n\n\nOUTPUT:\n\n");
+    // const cursor = await collection.find(findQuery).sort({ name: 1 });
+    const cursor = await collection.find().sort({ name: 1 });
     await cursor.forEach(credential => {
-      console.log(`${credential.user_email}`);
+      // console.log(`${credential.user_email}`);
+      queryDataJSON.push({user_id: credential.user_id, 
+                          user_email: credential.user_email, 
+                          user_password: credential.user_password, 
+                          user_type: credential.user_type});
     });
     // add a linebreak
-    console.log();
+    // console.log();
    } catch (err) {
     console.error(`Something went wrong trying to find the documents: ${err}\n`);
    }
 
+  //  console.log(queryDataJSON);
    await client.close();
+
+   return queryDataJSON;
+
+   
 }
-run().catch(console.dir);
+
+
+async function finalFunction() {
+  try {
+    let queryJSON = await run();
+    return queryJSON;
+    // console.log("heeee");
+    // console.log(queryDataJSON);
+
+  } catch (error) {
+    console.dir(error);
+    // Handle errors here.
+  }
+}
+
+module.exports = finalFunction; 
+
+
--- truffle/temp_for_helia_checking.js
+++ truffle/temp_for_helia_checking.js
@@ -1,20 +1,37 @@
 import { createHelia } from 'helia'
 import { strings } from '@helia/strings'
 import { json } from '@helia/json'
+import path from 'path';
+import { fileURLToPath } from 'url';
+import finalFunction from './atlas/app_trial_two.js';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// console.log(__dirname);
 
 
-const helia = await createHelia()
-const s = strings(helia)
+async function importData() {
+    let jsonData = await finalFunction();
+    console.log("hereeeee!!!");
+    console.log(jsonData);
+}
 
-const myImmutableAddress = await s.add('hello world')
-console.log(typeof myImmutableAddress)
+importData();
 
-console.log(await s.get(myImmutableAddress))
+//console.log(jsonData);
+
+// const helia = await createHelia()
+// const s = strings(helia)
+
+// const myImmutableAddress = await s.add('hello world')
+// console.log(typeof myImmutableAddress)
+
+// console.log(await s.get(myImmutableAddress))
 
 
-const j = json(helia)
+// const j = json(helia)
 
-const myImmutableAddress2 = await j.add({ hello: 'world' })
+// const myImmutableAddress2 = await j.add({ hello: 'world' })
 
-console.log(await j.get(myImmutableAddress2))
-// { hello: 'world' }
+// console.log(await j.get(myImmutableAddress2))
+// // { hello: 'world' }
