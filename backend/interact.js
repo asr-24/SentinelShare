893ba68be55fd8c88b675803a0d768fef6de96c5
Arushi <arushisr24@gmail.com>
2023-10-19 16:44:48 +0530

    integrate the blockchain with IPFS

--- backend/interact.js
+++ backend/interact.js
@@ -1,72 +1,102 @@
 import Web3 from "web3";
 import HDWalletProvider from "truffle-hdwallet-provider";
-import contractABI from "./build/contracts/AuthenticationLogging.json" assert { type: "json" };
-const { abi } = contractABI; // Extract the abi from the JSON object
-const contractAddress = "0xc6bbd4B6077D43dc11ddc66c951ea2677D7B999e";
 import { createHelia } from "helia";
 import { json } from "@helia/json";
 
-// Configure your Helia instance
-const configureHelia = async () => {
-  const helia = await createHelia();
-  return json(helia);
-};
+const sepoliaRpcUrl =
+  "https://sepolia.infura.io/v3/c557efceb61a41e7b11d471996a3f416";
 
-const web3 = new Web3(
-  new HDWalletProvider(
-    "drop spot bracket have piece canal domain ghost remind sword insect hero",
-    "https://sepolia.infura.io/v3/c557efceb61a41e7b11d471996a3f416",
-    30000
-  )
-);
+const mnemonic =
+  "drop spot bracket have piece canal domain ghost remind sword insect hero";
 
-const contract = new web3.eth.Contract(abi, contractAddress);
+import contractJSON from "./build/contracts/AuthenticationLogging.json" assert { type: "json" };
+const contractABI = contractJSON.abi;
+console.log(contractABI);
 
-const addLog = async (logData) => {
+const contractAddress = "0xc6bbd4B6077D43dc11ddc66c951ea2677D7B999e";
+
+// Function to initialize web3
+async function initializeWeb3() {
+  const provider = new HDWalletProvider(mnemonic, sepoliaRpcUrl);
+  const web3 = new Web3(provider);
+  return web3;
+}
+
+// Function to interact with the contract
+async function interactWithContract(logData) {
   try {
-    // Convert the data to a JSON string
-    const dataString = JSON.stringify(logData);
+    const web3 = await initializeWeb3();
 
-    console.log(dataString);
+    // Initializing the contract instance
+    const contract = new web3.eth.Contract(contractABI, contractAddress);
 
-    console.log("1 - ");
+    // Encoding the data for the storeData function
+    const encodedData = contract.methods.storeData(logData).encodeABI();
 
-    let accounts; // Declare accounts variable
+    // Getting the default account from the HDWalletProvider
+    const defaultAccount = web3.currentProvider.addresses[0];
+    // web3.eth.getAccounts().then(console.log);
+    console.log(defaultAccount); //is correct
 
-    const { ethereum } = window;
+    // Creating a transaction object
+    const transactionObject = {
+      from: defaultAccount,
+      to: contractAddress,
+      data: encodedData,
+    };
 
-    try {
-      accounts = await ethereum.request({ method: "eth_accounts" }); // Retrieve Ethereum accounts
-      console.log("Ethereum Accounts:", accounts);
-    } catch (error) {
-      console.error("Error getting Ethereum accounts:", error);
+    async function checkBalance() {
+      console.log("i am here -");
+      try {
+        const balance = await web3.eth.getBalance(
+          "0x407d73d8a49eeb85d32cf465507dd71d507100c1"
+        );
+        console.log("Balance:", balance);
+      } catch (error) {
+        console.error("Error:", error);
+      }
     }
 
-    console.log("2 - ");
-    const gas = await contract.methods.storeData(dataString).estimateGas();
-    console.log("3 - ");
-    const tx = await contract.methods.storeData(dataString).send({
-      from: accounts[0], // Use the first Ethereum account
-      gas: gas,
-    });
+    checkBalance();
 
-    console.log("Transaction Hash:", tx.transactionHash);
+    console.log(transactionObject);
+    // Sign and send the transaction
+    try {
+      console.log("at least i came here");
+      const transaction = await web3.eth.sendTransaction(transactionObject);
+      console.log("here here");
+      console.log("Transaction Sent:", transaction);
+      console.log("Transaction Hash:", transaction.transactionHash);
+      return transaction.transactionHash; 
+    } catch (error) {
+      console.error("Error Sending Transaction:", error);
+    }
 
-    return tx.transactionHash;
+    // console.log("wow");
+    // console.log(transaction);
+
+    
   } catch (error) {
-    console.error("Error sending transaction:", error);
-    throw error;
+    console.error("Error interacting with contract:", error);
   }
-};
+}
 
 (async () => {
-  const j = await configureHelia();
+  const helia = await createHelia();
+  const j = json(helia);
 
-  const transactionHash = await addLog({
-    user_id: "1001",
-    timestamp: "2023-10-03",
-    status: "success",
+  const user_id = "1001";
+  const timestamp = "2023-10-10";
+  const auth = "success";
+
+  const logData = JSON.stringify({
+    user_id: user_id,
+    timestamp: timestamp,
+    status: auth,
   });
+  
+  const transactionHash = await interactWithContract(logData);
+
   console.log("Transaction sent. Transaction Hash:", transactionHash);
 
   // Store CID on Helia
